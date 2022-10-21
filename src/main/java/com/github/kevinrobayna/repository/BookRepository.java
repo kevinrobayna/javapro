88da7f8d32ce400a2ceec6f0223f042007fba89d
Kevin Robayna <me@kevinrobayna.com>
2022-10-21 22:02:35 +0100

    Add auto generated method
    I'm using this to show the compilation time query generation

--- src/main/java/com/github/kevinrobayna/repository/BookRepository.java
+++ src/main/java/com/github/kevinrobayna/repository/BookRepository.java
@@ -6,8 +6,13 @@ import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
-@JdbcRepository(dialect= Dialect.POSTGRES)
-public interface BookRepository extends CrudRepository<Book, Long> { //
+import java.util.List;
+
+@JdbcRepository(dialect = Dialect.POSTGRES)
+public interface BookRepository extends CrudRepository<Book, Long> {
     @Executable
     Book find(String title);
+
+    @Executable
+    List<Book> findAllByTitle(String t);
 }
